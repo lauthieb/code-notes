ffac8fdf07cb3c4bb9048fe9fbcb479cd4bf916e
Laurent THIEBAULT <thiebault.laurent@gmail.com>
2018-01-20 11:10:48 +0100

    init(project): electron-vue

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: 'airbnb-base',
+  globals: {
+    __static: true
+  },
+  plugins: [
+    'html'
+  ],
+  'rules': {
+    'global-require': 0,
+    'import/no-unresolved': 0,
+    'no-param-reassign': 0,
+    'no-shadow': 0,
+    'import/extensions': 0,
+    'import/newline-after-import': 0,
+    'no-multi-assign': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
