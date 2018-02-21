c8215c4c4632f669fe3a1a9513f105d8f6bce3b6
Igor Guastalla <limaguastallaigor@gmail.com>
2018-02-21 17:41:26 -0300

    feature(tags): Added colored tags (#38)
    * Added new dependency (Color Hash) that is used to generate the colors of each tag.
    
    * Added color-hash in yarn.lock.

--- src/renderer/components/notes-list/note-card/NoteCard.vue
+++ src/renderer/components/notes-list/note-card/NoteCard.vue
@@ -3,6 +3,7 @@
 
 <script>
 import Vuex from 'vuex';
+import ColorHash from 'color-hash';
 import editor from '../../editor/Editor';
 import UpdateNoteModal from '../../modals/update-note-modal/UpdateNoteModal';
 import BTooltip from '../../../../../node_modules/buefy/src/components/tooltip/Tooltip.vue';
@@ -30,13 +31,19 @@ export default {
     },
   },
   methods: {
+    stringToColour (str) {
+      const colorHash = new ColorHash({lightness: 0.5, saturation: 0.6});
+      return colorHash.hex(str);
+    },
     updateNote() {
       this.$store.dispatch('updateNote', this.note);
     },
     deleteNote() {
       this.$dialog.confirm({
         title: this.gistsSelected ? 'Delete gist' : 'Delete note',
-        message: `Are you sure you want to delete this ${this.gistsSelected ? 'gist': 'note'} ?`,
+        message: `Are you sure you want to delete this ${
+          this.gistsSelected ? 'gist' : 'note'
+        } ?`,
         confirmText: 'Delete',
         type: 'is-danger',
         hasIcon: true,
