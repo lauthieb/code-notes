b931e5ec7b637bcae50e4e74d6c93c23975a8051
Laurent Thiebault <thiebault.laurent@gmail.com>
2019-10-07 22:42:34 +0200

    revert multispinner

--- .electron-vue/build.js
+++ .electron-vue/build.js
@@ -7,7 +7,7 @@ const chalk = require('chalk')
 const del = require('del')
 const { spawn } = require('child_process')
 const webpack = require('webpack')
-
+const Multispinner = require('multispinner')
 
 const mainConfig = require('./webpack.main.config')
 const rendererConfig = require('./webpack.renderer.config')
@@ -34,6 +34,10 @@ function build () {
   del.sync(['dist/electron/*', '!.gitkeep'])
 
   const tasks = ['main', 'renderer']
+  const m = new Multispinner(tasks, {
+    preText: 'building',
+    postText: 'process'
+  })
 
   let results = ''
 
