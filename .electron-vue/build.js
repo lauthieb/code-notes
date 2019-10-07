a0a83f934399deff40a6ad7a2d88f55884bbedcc
Laurent Thiebault <thiebault.laurent@gmail.com>
2019-10-07 21:50:35 +0200

    fix: vulnerabilities

--- .electron-vue/build.js
+++ .electron-vue/build.js
@@ -7,7 +7,6 @@ const chalk = require('chalk')
 const del = require('del')
 const { spawn } = require('child_process')
 const webpack = require('webpack')
-const Multispinner = require('multispinner')
 
 
 const mainConfig = require('./webpack.main.config')
@@ -35,10 +34,6 @@ function build () {
   del.sync(['dist/electron/*', '!.gitkeep'])
 
   const tasks = ['main', 'renderer']
-  const m = new Multispinner(tasks, {
-    preText: 'building',
-    postText: 'process'
-  })
 
   let results = ''
 
